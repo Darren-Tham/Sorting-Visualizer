216b7abf691f6ab1343cf418063158509e22f369
Darren Tham <darrent9859@gmail.com>
2022-07-02 21:32:32 -0400

    Added spacing between bars

--- src/components/bar-component/Bars.jsx
+++ src/components/bar-component/Bars.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import Bar from './Bar'
 
 export default function Bars(props) {
-  const barWidth = 100 / props.arr.length
-  const heightPercentage = getHeight() / 100
+  const body = document.body
+  const spacing = getSpacing(body)
+  const barWidth = getBarWidth(props.arr.length, spacing)
+  const heightPercentage = getMaxBarHeight(body) / 100
+
   return (
     <div>
-      {props.arr.map((num, idx) => <Bar key={idx} width={barWidth} height={heightPercentage * num} left={barWidth * idx} num={num} />)}
+      {props.arr.map((num, idx) => <Bar key={idx} width={barWidth} height={heightPercentage * num} left={(barWidth + spacing) * idx} num={num} />)}
     </div>
   )
 }
 
-function getHeight() {
-  const body = document.querySelector('body')
+function getBarWidth(length, spacing) {
+  return (100 - spacing * (length  - 1)) / length
+}
+
+function getSpacing(body) {
+  const spacing = getComputedStyle(body).getPropertyValue('--spacing')
+  return parseFloat(spacing)
+}
+
+function getMaxBarHeight(body) {
   const textContainerHeight = getComputedStyle(body).getPropertyValue('--text-container-height')
   return 100 - parseInt(textContainerHeight)
 }
